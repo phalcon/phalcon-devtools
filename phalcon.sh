5ed424269fc7e89d3bfe75f6733c0f3f8f363d1e
phalcon <andres@phalconphp.com>
2012-04-18 16:01:01 -0500

    Indenting and changing exit with return

--- phalcon.sh
+++ phalcon.sh
@@ -3,24 +3,26 @@
 if [ -z "$PTOOLSPATH" ]; then
         if [ "$0" == "-bash" ];
         then
-        echo "Phalcon devtools install. PhalconPHP 2012. Make sure phalcon.sh is in the same dir as phalcon.php and that you run this with sudo or as root."
-        echo "Installing Devtools..."
-        DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-        export PTOOLSPATH=$DIR
-        export PATH=$PATH:$DIR
-        echo "Working dir is: "$DIR
+        	echo "Phalcon Developer Tools Installer"
+        	echo "Make sure phalcon.sh is in the same dir as phalcon.php and that you are running this with sudo or as root."
+        	echo "Installing Devtools..."
+        	DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+        	export PTOOLSPATH=$DIR
+        	export PATH=$PATH:$DIR
+        	echo "Working dir is: "$DIR
         else
-        echo 'Run this installer with ". ./phalcon.sh". Exiting...'
-        exit
+        	echo 'Run this installer with ". ./phalcon.sh". Exiting...'
+        	return
         fi
 fi
+
 app="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 if [ ! -L $app/phalcon ]; then
-        echo "Generating symlink"
+        echo "Generating symlink..."
         ln -s $app/phalcon.sh $app/phalcon
         chmod +x $app/phalcon
         echo "Done. Devtools installed!"
 else
-php "$PTOOLSPATH/phalcon.php" $*
+	php "$PTOOLSPATH/phalcon.php" $*
 fi
 
