92e06ff0d97c103d16479f4c39a84922296a1ecc
Wojtek Gancarczyk <gancarczyk@gmail.com>
2012-09-08 18:36:08 +0200

    fixed directory paths

--- templates/project/config.php
+++ templates/project/config.php
@@ -7,11 +7,11 @@ return new \Phalcon\Config(array(
 		'name'     => 'phalcon',
 	),
 	'phalcon' => array(
-		'controllersDir' => __DIR__ . '/../app/controllers/',
-		'modelsDir'      => __DIR__ . '/../app/models/',
-		'viewsDir'       => __DIR__ . '/../app/views/',
-		'pluginsDir'     => __DIR__ . '/../app/plugins/',
-		'libraryDir'     => __DIR__ . '/../app/library/',
+		'controllersDir' => __DIR__ . '/../../app/controllers/',
+		'modelsDir'      => __DIR__ . '/../../app/models/',
+		'viewsDir'       => __DIR__ . '/../../app/views/',
+		'pluginsDir'     => __DIR__ . '/../../app/plugins/',
+		'libraryDir'     => __DIR__ . '/../../app/library/',
 		'baseUri'        => '/@@name@@/',
 	),
 	'metadata' => array(
