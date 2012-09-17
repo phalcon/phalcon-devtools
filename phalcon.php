8ae4c97e3d0c0ebbfc607d6e44ffcd2efafa1dbc
Phalcon <andres@phalconphp.com>
2012-09-16 19:28:03 -0500

    Moving builtin commands to Phalcon/Commands/Builtin/

--- phalcon.php
+++ phalcon.php
@@ -25,48 +25,44 @@ use Phalcon\Script;
 use Phalcon\Script\Color;
 use Phalcon\Version as Version;
 
-$loader = new \Phalcon\Loader();
-
-$loader->registerDirs(array(
-    __DIR__ . '/scripts/'
-))
-
-$loader->registerNamespaces(array(
-    'Phalcon' => __DIR__.'/scripts/'
-));
-
-$loader->register();
-
-if (!extension_loaded('phalcon')) {
-	print Color::error('Phalcon extension isn\'t installed, follow these instructions to install it: http://phalconphp.com/documentation/install') . PHP_EOL;
-	die();
-}
-
-if (Version::getId()<'0050022') {
-    print Color::error('Your Phalcon version isn\'t compatible with Developer Tools, download the latest at: http://phalconphp.com/download') . PHP_EOL;
-    die();
-}
-
-if (!defined('TEMPLATES_PATH')) {
-    define('TEMPLATES_PATH', __DIR__ . '/templates');
-}
-
-$vendor = sprintf('Phalcon DevTools (%s)', Version::get());
-print PHP_EOL . Color::colorize($vendor, Color::FG_GREEN, Color::AT_BOLD) . PHP_EOL . PHP_EOL;
-
-$script = new Script();
-
-$script->attach(new \Phalcon\Command\Commands);
-$script->attach(new \Phalcon\Command\Controller);
-$script->attach(new \Phalcon\Command\Model);
-$script->attach(new \Phalcon\Command\Project);
-$script->attach(new \Phalcon\Command\Scaffold);
-
 try {
+
+	$loader = new \Phalcon\Loader();
+
+	$loader->registerDirs(array(
+		__DIR__ . '/scripts/'
+	));
+
+	$loader->registerNamespaces(array(
+		'Phalcon' => __DIR__.'/scripts/'
+	));
+
+	$loader->register();
+
+	if (!extension_loaded('phalcon')) {
+		throw new Exception('Phalcon extension isn\'t installed, follow these instructions to install it: http://phalconphp.com/documentation/install');
+	}
+
+	if (Version::getId() < Script::COMPATIBLE_VERSION) {
+		throw new Exception('Your Phalcon version isn\'t compatible with Developer Tools, download the latest at: http://phalconphp.com/download');
+	}
+
+	if (!defined('TEMPLATES_PATH')) {
+		define('TEMPLATES_PATH', __DIR__ . '/templates');
+	}
+
+	$vendor = sprintf('Phalcon DevTools (%s)', Version::get());
+	print PHP_EOL . Color::colorize($vendor, Color::FG_GREEN, Color::AT_BOLD) . PHP_EOL . PHP_EOL;
+
+	$script = new Script();
+
+	$script->loadBuiltInCommands();
+	$script->loadUserCommands();
+
 	$script->run();
 }
 catch (\Phalcon\Exception $e) {
-    print Color::error($e->getMessage()) . PHP_EOL;
+	print Color::error($e->getMessage()) . PHP_EOL;
 	print $e->getTraceAsString() . PHP_EOL;
 }
 catch (\Exception $e) {
