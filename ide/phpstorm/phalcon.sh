423f00065089daddbb3aff2e199615201070c94b
Gedas Lukošius <g3dut1s@gmail.com>
2014-01-08 02:09:15 +0200

    Updated year. Changed title for file DocBlock.

--- ide/phpstorm/phalcon.sh
+++ ide/phpstorm/phalcon.sh
@@ -4,7 +4,7 @@
 #  +------------------------------------------------------------------------+
 #  | Phalcon Framework                                                      |
 #  +------------------------------------------------------------------------+
-#  | Copyright (c) 2011-2012 Phalcon Team (http://www.phalconphp.com)       |
+#  | Copyright (c) 2011-2014 Phalcon Team (http://www.phalconphp.com)       |
 #  +------------------------------------------------------------------------+
 #  | This source file is subject to the New BSD License that is bundled     |
 #  | with this package in the file docs/LICENSE.txt.                        |
@@ -36,6 +36,6 @@ fi
 
 if [ ! -z "$PTOOLSPATH" ]; then
 	php "$PTOOLSPATH/phalcon.php" $*
-else 
+else
 	echo "Error: Add environment variable PTOOLSPATH to your .profile"
 fi
