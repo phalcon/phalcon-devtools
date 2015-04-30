ca2840fc81495bfa1aa7c9bafb9498f7049c610a
Serghei Iakovlev <sadhooklay@gmail.com>
2015-05-01 01:02:12 +0300

    Update bash completion

--- phalcon-completion.bash
+++ phalcon-completion.bash
@@ -1,5 +1,23 @@
-# Bash completion for phalcon-devtools,
-# https://github.com/phalcon/phalcon-devtools
+#!/usr/bin/env bash
+
+#
+#  +------------------------------------------------------------------------+
+#  | Phalcon Developer Tools                                                |
+#  +------------------------------------------------------------------------+
+#  | Copyright (c) 2011-2015 Phalcon Team (http://www.phalconphp.com)       |
+#  +------------------------------------------------------------------------+
+#  | This source file is subject to the New BSD License that is bundled     |
+#  | with this package in the file docs/LICENSE.txt.                        |
+#  |                                                                        |
+#  | If you did not receive a copy of the license and are unable to         |
+#  | obtain it through the world-wide-web, please send an email             |
+#  | to license@phalconphp.com so we can send you a copy immediately.       |
+#  +------------------------------------------------------------------------+
+#  | Authors: Andres Gutierrez <andres@phalconphp.com>                      |
+#  |          Eduar Carvajal <eduar@phalconphp.com>                         |
+#  |          Jakob <@jamurko>                                              |
+#  +------------------------------------------------------------------------+
+#
 
 _phalcon()
 {
@@ -7,7 +25,8 @@ _phalcon()
 	_get_comp_words_by_ref -n = cur prev
 
 	commands="commands list enumerate controller create-controller model \
-	create-model all-models create-all-models project create-project scaffold webtools"
+	create-model all-models create-all-models project create-project scaffold \
+	create-scaffold migration create-migration webtools create-webtools"
 
 	case "$prev" in
 		project|create-project)
