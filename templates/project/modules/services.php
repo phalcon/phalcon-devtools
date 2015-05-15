760544e1bd5ed5b7395b95de193c90a2b8ee7e4b
m4nu <m4nu.dev@gmail.com>
2015-05-15 02:13:08 -0500

    fix issue #402

--- templates/project/modules/services.php
+++ templates/project/modules/services.php
@@ -41,7 +41,7 @@ $di->set('url', function () {
  * Database connection is created based in the parameters defined in the configuration file
  */
 $di->set('db', function () use ($config) {
-    return new DbAdapter($config->toArray());
+    return new DbAdapter($config->database->toArray());
 });
 
 /**
