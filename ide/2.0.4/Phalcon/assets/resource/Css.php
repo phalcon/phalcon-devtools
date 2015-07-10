2a59783d531e34a503d4afe8d28f08ad877ee874
Serghei Iakovlev <sadhooklay@gmail.com>
2015-07-10 21:58:39 +0300

    Fixed #441

---
+++ ide/2.0.4/Phalcon/assets/resource/Css.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Phalcon\Assets\Resource;
+
+/**
+ * Phalcon\Assets\Resource\Css
+ * Represents CSS resources
+ */
+class Css extends \Phalcon\Assets\Resource
+{
+
+    /**
+     * Phalcon\Assets\Resource\Css
+     *
+     * @param string $path 
+     * @param boolean $local 
+     * @param boolean $filter 
+     * @param array $attributes 
+     */
+    public function __construct($path, $local = true, $filter = true, $attributes = null) {}
+
+}
