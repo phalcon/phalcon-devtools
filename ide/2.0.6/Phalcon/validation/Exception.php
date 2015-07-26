69dd4736030fdeff5c793953e40345868e91f09d
Serghei Iakovlev <sadhooklay@gmail.com>
2015-07-26 09:04:52 +0300

    Added 2.0.6 stubs

---
+++ ide/2.0.6/Phalcon/validation/Exception.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Phalcon\Validation;
+
+/**
+ * Phalcon\Validation\Exception
+ * Exceptions thrown in Phalcon\Validation\* classes will use this class
+ */
+class Exception extends \Phalcon\Exception
+{
+
+}
