f4e087d8654a22d801dcc0333e22422496a57582
Serghei Iakovlev <sadhooklay@gmail.com>
2015-09-26 00:26:54 +0300

    Added 2.0.8 stubs

---
+++ ide/2.0.8/Phalcon/cache/FrontendInterface.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Phalcon\Cache;
+
+/**
+ * Phalcon\Cache\FrontendInterface
+ * Interface for Phalcon\Cache\Frontend adapters
+ */
+interface FrontendInterface
+{
+
+    /**
+     * Returns the cache lifetime
+     *
+     * @return int 
+     */
+    public function getLifetime();
+
+    /**
+     * Check whether if frontend is buffering output
+     *
+     * @return bool 
+     */
+    public function isBuffering();
+
+    /**
+     * Starts the frontend
+     */
+    public function start();
+
+    /**
+     * Returns output cached content
+     *
+     * @return string 
+     */
+    public function getContent();
+
+    /**
+     * Stops the frontend
+     */
+    public function stop();
+
+    /**
+     * Serializes data before storing it
+     *
+     * @param mixed $data 
+     */
+    public function beforeStore($data);
+
+    /**
+     * Unserializes data after retrieving it
+     *
+     * @param mixed $data 
+     */
+    public function afterRetrieve($data);
+
+}
---
+++ ide/2.0.8/Phalcon/tag/Select.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Phalcon\Tag;
+
+/**
+ * Phalcon\Tag\Select
+ * Generates a SELECT html tag using a static array of values or a Phalcon\Mvc\Model resultset
+ */
+abstract class Select
+{
+
+    /**
+     * Generates a SELECT tag
+     *
+     * @param array $parameters 
+     * @param array $data 
+     */
+    public static function selectField($parameters, $data = null) {}
+
+    /**
+     * Generate the OPTION tags based on a resultset
+     *
+     * @param \Phalcon\Mvc\Model\Resultset $resultset 
+     * @param array $using 
+     * @param mixed $value 
+     * @param string $closeOption 
+     */
+    private static function _optionsFromResultset($resultset, $using, $value, $closeOption) {}
+
+    /**
+     * Generate the OPTION tags based on an array
+     *
+     * @param array $data 
+     * @param mixed $value 
+     * @param string $closeOption 
+     */
+    private static function _optionsFromArray($data, $value, $closeOption) {}
+
+}
