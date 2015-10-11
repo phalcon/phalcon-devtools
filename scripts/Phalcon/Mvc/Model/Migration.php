21b5c1363705c403e7bab3d00df546376b22e3b0
Mateusz Błaszczyk <mateusz.blaszczyk@live.com>
2015-10-11 15:30:18 +0200

    revert

--- scripts/Phalcon/Mvc/Model/Migration.php
+++ scripts/Phalcon/Mvc/Model/Migration.php
@@ -493,13 +493,7 @@ class Migration
      */
     public function morphTable($tableName, $definition)
     {
-        if(self::$_databaseConfig->get('adapter') === 'Postgresql'){
-            $defaultSchema = self::$_databaseConfig->get('dbname').'.'.self::$_databaseConfig->get('schema');
-        }
-        else{
-            $defaultSchema = self::$_databaseConfig->get('dbname');
-        }
-
+        $defaultSchema = self::$_databaseConfig->get('dbname');
         $tableExists = self::$_connection->tableExists($tableName, $defaultSchema);
 
         if (isset($definition['columns'])) {
