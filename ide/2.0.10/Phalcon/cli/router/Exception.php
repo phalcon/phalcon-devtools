560870d78f9227990667a1d3a994a2819cfa4bd2
Serghei Iakovlev <sadhooklay@gmail.com>
2016-02-05 09:06:48 +0200

    Added 2.0.10 stubs

---
+++ ide/2.0.10/Phalcon/cli/router/Exception.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Phalcon\Cli\Router;
+
+/**
+ * Phalcon\Cli\Router\Exception
+ * Exceptions thrown in Phalcon\Cli\Router will use this class
+ */
+class Exception extends \Phalcon\Exception
+{
+
+}
