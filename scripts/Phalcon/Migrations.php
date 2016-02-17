f22e53847eb2413add78b10412fd928c3dc0454a
Leonid Sysoletin <l.sysoletin@rambler-co.ru>
2016-02-17 11:29:39 +0300

    keep last migration number in database

--- scripts/Phalcon/Migrations.php
+++ scripts/Phalcon/Migrations.php
@@ -25,6 +25,7 @@ use Phalcon\Script\Color;
 use Phalcon\Version\Item as VersionItem;
 use Phalcon\Mvc\Model\Migration as ModelMigration;
 use Phalcon\Mvc\Model\Exception as ModelException;
+use Phalcon\Db\Exception as DbException;
 use Phalcon\Script\ScriptException;
 use DirectoryIterator;
 
@@ -35,6 +36,24 @@ use DirectoryIterator;
  */
 class Migrations
 {
+    /**
+     * Migration database connection
+     * @var \Phalcon\Db\AdapterInterface
+     */
+    protected static $_connection;
+
+    /**
+     * Configuration holder
+     * @var \Phalcon\Config
+     */
+    protected static $_config;
+
+    /**
+     * Configuration holder
+     * @var \Phalcon\Config
+     */
+    protected static $_migrationFid;
+
     /**
      * Generate migrations
      *
@@ -93,15 +112,17 @@ class Migrations
         if ($tableName == 'all') {
             $migrations = ModelMigration::generateAll($version, $exportData);
             foreach ($migrations as $tableName => $migration) {
-                file_put_contents($migrationsDir.'/'.$version.'/'.$tableName.'.php', '<?php '.PHP_EOL.PHP_EOL.$migration);
+                file_put_contents($migrationsDir . '/' . $version . '/' . $tableName . '.php',
+                    '<?php ' . PHP_EOL . PHP_EOL . $migration);
             }
         } else {
             $migration = ModelMigration::generate($version, $tableName, $exportData);
-            file_put_contents($migrationsDir.'/'.$version.'/'.$tableName.'.php', '<?php '.PHP_EOL.PHP_EOL.$migration);
+            file_put_contents($migrationsDir . '/' . $version . '/' . $tableName . '.php',
+                '<?php ' . PHP_EOL . PHP_EOL . $migration);
         }
 
         if (self::isConsole()) {
-            print Color::success('Version '.$version.' was successfully generated').PHP_EOL;
+            print Color::success('Version ' . $version . ' was successfully generated') . PHP_EOL;
         }
     }
 
@@ -133,7 +154,7 @@ class Migrations
             throw new ModelException('Migrations directory could not found.');
         }
 
-        $config = $options['config'];
+        self::$_config = $config = $options['config'];
         if (!$config instanceof Config) {
             throw new ModelException('Internal error. Config should be instance of \Phalcon\Config');
         }
@@ -159,17 +180,13 @@ class Migrations
         }
 
         // read current version
-        if (is_file($path.'.phalcon')) {
-            unlink($path.'.phalcon');
-            mkdir($path.'.phalcon');
+        if (is_file($path . '.phalcon')) {
+            unlink($path . '.phalcon');
+            mkdir($path . '.phalcon');
+            chmod($path . '.phalcon', 0775);
         }
 
-        $migrationFid = $path.'.phalcon/migration-version';
-        $initialVersion = new VersionItem(file_exists($migrationFid) ? file_get_contents($migrationFid) : null);
-
-        if ($initialVersion->getStamp() == $finalVersion->getStamp()) {
-            return; // nothing to do
-        }
+        self::$_migrationFid = $path . '.phalcon/migration-version';
 
         // init ModelMigration
         if (!isset($config->database)) {
@@ -179,11 +196,18 @@ class Migrations
         ModelMigration::setup($config->database);
         ModelMigration::setMigrationPath($migrationsDir);
 
+        $initialVersion = self::getCurrentVersion();
+
+        if ($initialVersion->getStamp() == $finalVersion->getStamp()) {
+            return; // nothing to do
+        }
+
         $direction = ModelMigration::DIRECTION_FORWARD;
         if ($finalVersion->getStamp() < $initialVersion->getStamp()) {
             $direction = ModelMigration::DIRECTION_BACK;
         }
         // run migration
+        $lastGoodVersion = $initialVersion;
         $versionsBetween = VersionItem::between($initialVersion, $finalVersion, $versions);
         foreach ($versionsBetween as $version) {
             if ($tableName == 'all') {
@@ -199,10 +223,75 @@ class Migrations
                 ModelMigration::migrate($initialVersion, $version, $tableName, $direction);
             }
 
-            file_put_contents($migrationFid, (string)$version);
+            self::setCurrentVersion((string)$lastGoodVersion, (string)$version);
+            $lastGoodVersion = $version;
             print Color::success('Version ' . $version . ' was successfully migrated');
 
             $initialVersion = $version;
+        }
+    }
+
+    /**
+     * @param \Phalcon\Config $database
+     * @throws DbException
+     */
+    public static function connSetup($database)
+    {
+        if (!isset($database->adapter)) {
+            throw new DbException('Unspecified database Adapter in your configuration!');
+        }
+
+        $adapter = '\\Phalcon\\Db\\Adapter\\Pdo\\' . $database->adapter;
+
+        if (!class_exists($adapter)) {
+            throw new DbException('Invalid database Adapter!');
+        }
+
+        $configArray = $database->toArray();
+        unset($configArray['adapter']);
+        self::$_connection = new $adapter($configArray);
+
+        if ($database->adapter == 'Mysql') {
+            self::$_connection->query('SET FOREIGN_KEY_CHECKS=0');
+        }
+
+    }
+
+    public static function getCurrentVersion()
+    {
+        if (isset(self::$_config->migrationsLog) && ('database' == self::$_config->migrationsLog)) {
+            self::connSetup(self::$_config->database);
+
+            if (!self::$_connection->tableExists('phalcon_migrations')) {
+                self::$_connection->execute('CREATE TABLE `phalcon_migrations` (`version` VARCHAR(14) NOT NULL, `start_time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, `end_time` TIMESTAMP NOT NULL DEFAULT "0000-00-00 00:00:00" ) DEFAULT CHARSET = utf8;');
+                self::$_connection->execute('INSERT INTO `phalcon_migrations` (`version`) VALUES ("0.0.0");');
+                $initialVersion = new VersionItem('0.0.0');
+            } else {
+                $initialVersion = self::$_connection->query('SELECT `version` FROM `phalcon_migrations` ORDER BY `version` DESC LIMIT 1;');
+                if (0 == $initialVersion->numRows()) {
+                    self::$_connection->execute('INSERT INTO `phalcon_migrations` (`version`) VALUES ("0.0.0");');
+                    $initialVersion = new VersionItem('0.0.0');
+                } else {
+                    $initialVersion = $initialVersion->fetch();
+                    $initialVersion = new VersionItem($initialVersion['version']);
+                }
+            }
+
+            self::$_connection->execute('UPDATE `phalcon_migrations` SET `start_time`=NOW()  WHERE `version`="' . (string)$initialVersion . '" LIMIT 1;');
+
+        } else {
+            $initialVersion = new VersionItem(file_exists(self::$_migrationFid) ? file_get_contents(self::$_migrationFid) : null);
+        }
+
+        return $initialVersion;
+    }
+
+    public static function setCurrentVersion($lastVersion, $currentVersion)
+    {
+        if (isset(self::$_config->migrationsLog) && ('database' == self::$_config->migrationsLog)) {
+            self::$_connection->execute('UPDATE `phalcon_migrations` SET `version`="' . (string)$currentVersion . '", `end_time`=NOW()  WHERE `version`="' . (string)$lastVersion . '" LIMIT 1;');
+        } else {
+            file_put_contents(self::$_migrationFid, (string)$currentVersion);
         }
     }
 }
