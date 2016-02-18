bbadb79ca1fedf30298f6b3231359e55f9f5992e
Ivan Zinovyev <vanyazin@gmail.com>
2016-02-18 08:48:01 +0300

    Shallow command refactoring

--- scripts/Phalcon/Commands/Builtin/Migration.php
+++ scripts/Phalcon/Commands/Builtin/Migration.php
@@ -49,7 +49,7 @@ class Migration extends Command
             'directory=s'       => 'Directory where the project was created',
             'table=s'           => 'Table to migrate. Default: all',
             'version=s'         => 'Version to migrate',
-            'name=s'            => 'Migration version description (Will generate timestamp 3prefixed name)',
+            'descr=s'           => 'Migration version description (new-style migration naming)',
             'force'             => 'Forces to overwrite existing migrations',
             'no-auto-increment' => 'Disable auto increment (Generating only)',
             'data=s'            => 'Export data [always|oncreate] (Import data when run migration)',
@@ -64,68 +64,67 @@ class Migration extends Command
      */
     public function run(array $parameters)
     {
-        if ($this->isReceivedOption('table')) {
-            $tableName = $this->getOption('table');
-        } else {
-            $tableName = 'all';
-        }
+        // Build path to the core app files
+        $path = $this->isReceivedOption('directory') ? $this->getOption('directory') : '';
+        $path = realpath($path).DIRECTORY_SEPARATOR;
 
-        $path = '';
-        if ($this->isReceivedOption('directory')) {
-            $path = $this->getOption('directory');
-        }
-
-        $path = realpath($path) . DIRECTORY_SEPARATOR;
-
+        // Load configuration
         if ($this->isReceivedOption('config')) {
-            $config = $this->loadConfig($path . $this->getOption('config'));
+            $config = $this->loadConfig($path.$this->getOption('config'));
         } else {
             $config = $this->getConfig($path);
         }
 
+        // Look for migrations
         if ($this->isReceivedOption('migrations')) {
             $migrationsDir = $path.$this->getOption('migrations');
         } elseif (isset($config['application']['migrationsDir'])) {
             $migrationsDir = $config['application']['migrationsDir'];
             if (!$this->path->isAbsolutePath($migrationsDir)) {
-                $migrationsDir = $path . $migrationsDir;
+                $migrationsDir = $path.$migrationsDir;
             }
+        } elseif (file_exists($path.'app')) {
+            $migrationsDir = $path.'app/migrations';
+        } elseif (file_exists($path.'apps')) {
+            $migrationsDir = $path.'apps/migrations';
         } else {
-            if (file_exists($path.'app')) {
-                $migrationsDir = $path.'app/migrations';
-            } elseif (file_exists($path.'apps')) {
-                $migrationsDir = $path.'apps/migrations';
-            } else {
-                $migrationsDir = $path.'migrations';
-            }
+            $migrationsDir = $path.'migrations';
         }
 
+        // Load other optins
+        $tableName = $this->isReceivedOption('table') ? $tableName = $this->getOption('table') : 'all';
         $exportData = $this->getOption('data');
-        $name = $this->getOption('name');
+        $descr = $this->getOption('descr');
         $action = $this->getOption(array('action', 1));
         $version = $this->getOption('version');
 
         if ($action == 'generate') {
-            Migrations::generate(array(
-                'directory'       => $path,
-                'tableName'       => $tableName,
-                'exportData'      => $exportData,
-                'migrationsDir'   => $migrationsDir,
-                'originalVersion' => $version,
-                'force'           => $this->isReceivedOption('force'),
-                'no-ai'           => $this->isReceivedOption('no-auto-increment'),
-                'config'          => $config
-            ));
+            Migrations::generate(
+                array(
+                    'directory'       => $path,
+                    'tableName'       => $tableName,
+                    'exportData'      => $exportData,
+                    'migrationsDir'   => $migrationsDir,
+                    'originalVersion' => $version,
+                    'force'           => $this->isReceivedOption('force'),
+                    'no-ai'           => $this->isReceivedOption('no-auto-increment'),
+                    'config'          => $config,
+                    'descr'           => $descr,
+                )
+            );
         } else {
             if ($action == 'run') {
-                Migrations::run(array(
-                    'directory'     => $path,
-                    'tableName'     => $tableName,
-                    'migrationsDir' => $migrationsDir,
-                    'force'         => $this->isReceivedOption('force'),
-                    'config'        => $config,
-                    'version'       => $version,
-                ));
+                Migrations::run(
+                    array(
+                        'directory'     => $path,
+                        'tableName'     => $tableName,
+                        'migrationsDir' => $migrationsDir,
+                        'force'         => $this->isReceivedOption('force'),
+                        'config'        => $config,
+                        'version'       => $version,
+                        'descr'         => $descr,
+                    )
+                );
             }
         }
     }
@@ -147,18 +146,18 @@ class Migration extends Command
      */
     public function getHelp()
     {
-        print Color::head('Help:') . PHP_EOL;
-        print Color::colorize('  Generates/Run a Migration') . PHP_EOL . PHP_EOL;
+        print Color::head('Help:').PHP_EOL;
+        print Color::colorize('  Generates/Run a Migration').PHP_EOL.PHP_EOL;
 
-        print Color::head('Usage: Generate a Migration') . PHP_EOL;
-        print Color::colorize('  migration generate', Color::FG_GREEN) . PHP_EOL . PHP_EOL;
+        print Color::head('Usage: Generate a Migration').PHP_EOL;
+        print Color::colorize('  migration generate', Color::FG_GREEN).PHP_EOL.PHP_EOL;
 
-        print Color::head('Usage: Run a Migration') . PHP_EOL;
-        print Color::colorize('  migration run', Color::FG_GREEN) . PHP_EOL . PHP_EOL;
+        print Color::head('Usage: Run a Migration').PHP_EOL;
+        print Color::colorize('  migration run', Color::FG_GREEN).PHP_EOL.PHP_EOL;
 
-        print Color::head('Arguments:') . PHP_EOL;
+        print Color::head('Arguments:').PHP_EOL;
         print Color::colorize('  help', Color::FG_GREEN);
-        print Color::colorize("\tShows this help text") . PHP_EOL . PHP_EOL;
+        print Color::colorize("\tShows this help text").PHP_EOL.PHP_EOL;
 
         $this->printParameters($this->getPossibleParams());
     }
