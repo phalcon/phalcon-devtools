7e9e6c680761411ef5e462bca47e16f0c7ad3385
Serghei Iakovlev <serghei@phalconphp.com>
2016-08-10 00:49:33 +0300

    Updated copyright

--- scripts/Phalcon/Commands/Command.php
+++ scripts/Phalcon/Commands/Command.php
@@ -4,7 +4,7 @@
   +------------------------------------------------------------------------+
   | Phalcon Developer Tools                                                |
   +------------------------------------------------------------------------+
-  | Copyright (c) 2011-2015 Phalcon Team (http://www.phalconphp.com)       |
+  | Copyright (c) 2011-2016 Phalcon Team (http://www.phalconphp.com)       |
   +------------------------------------------------------------------------+
   | This source file is subject to the New BSD License that is bundled     |
   | with this package in the file docs/LICENSE.txt.                        |
@@ -34,9 +34,9 @@ use Phalcon\Config;
 /**
  * Command Class
  *
- * @package     Phalcon\Commands
- * @copyright   Copyright (c) 2011-2015 Phalcon Team (team@phalconphp.com)
- * @license     New BSD License
+ * @package   Phalcon\Commands
+ * @copyright Copyright (c) 2011-2016 Phalcon Team (team@phalconphp.com)
+ * @license   New BSD License
  */
 abstract class Command implements CommandsInterface
 {
