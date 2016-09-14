6418168d8bee1fd8b05299ec351d88802893af2c
Serghei Iakovlev <serghei@phalconphp.com>
2016-09-14 23:39:36 +0300

    Added Volt\Extension\Php::compileFunction

--- scripts/Phalcon/Mvc/View/Engine/Volt/Extension/Php.php
+++ scripts/Phalcon/Mvc/View/Engine/Volt/Extension/Php.php
@@ -21,6 +21,36 @@
 
 namespace Phalcon\Mvc\View\Engine\Volt\Extension;
 
+/**
+ * \Phalcon\Mvc\View\Engine\Volt\Extension\Php
+ *
+ * Allows to use any PHP function in Volt.
+ *
+ * @package Phalcon\Mvc\View\Engine\Volt\Extension
+ */
 class Php
 {
+    /**
+     * Tries to compile native PHP function.
+     *
+     * Invoked before any attempt to compile a function call in any template.
+     *
+     * @param string $name
+     * @param mixed  $arguments
+     *
+     * @return null|string
+     */
+    public function compileFunction($name, $arguments = null)
+    {
+        if (function_exists($name)) {
+            if (func_num_args() > 1) {
+                $arguments = array_slice(func_get_args(), 1);
+                return $name . '(' . $arguments[0] . ')';
+            }
+
+            return $name . '()';
+        }
+
+        return null;
+    }
 }
