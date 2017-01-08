fff5583de034f45fb3b429eee2240f1277e0dcf7
Roger <rogervila@me.com>
2017-01-08 20:48:08 +0100

    maintain single quotes

--- templates/project/simple/index.php
+++ templates/project/simple/index.php
@@ -17,7 +17,7 @@ try {
     /**
      * Read services
      */
-    include APP_PATH . "/config/services.php";
+    include APP_PATH . '/config/services.php';
 
     /**
      * Get config service for use in inline setup below
