31e9d89e343baeadacc5d44907a57399516c488f
Serghei Iakovlev <serghei@phalconphp.com>
2017-04-02 01:09:47 +0300

    Fixed issue #1001

--- bootstrap/autoload.php
+++ bootstrap/autoload.php
@@ -100,7 +100,7 @@ defined('HOSTNAME') || define('HOSTNAME', explode('.', gethostname())[0]);
 /**
  * @const ADMIN_LTE_VERSION The AdminLTE version.
  */
-define('ADMIN_LTE_VERSION', '2.3.6');
+defined('ADMIN_LTE_VERSION') || define('ADMIN_LTE_VERSION', '2.3.6');
 
 /** @const COMPATIBLE_VERSION The compatible Phalcon version. */
 define('COMPATIBLE_VERSION', 3000040);
