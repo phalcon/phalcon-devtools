00feb847fbc69843db7fbe1a775b1b40e8ccaa62
twentychung <vgocoder@users.noreply.github.com>
2017-04-20 18:05:43 +0800

    Update Model.php
    Phalcon Developer Tools and Validation are incompatible

--- scripts/Phalcon/Builder/Model.php
+++ scripts/Phalcon/Builder/Model.php
@@ -27,7 +27,7 @@ use Phalcon\Db\Column;
 use Phalcon\Validation;
 use Phalcon\Generator\Snippet;
 use Phalcon\Db\ReferenceInterface;
-use Phalcon\Mvc\Model\Validator\Email as EmailValidator;
+use Phalcon\Validation\Validator\Email as EmailValidator;
 
 /**
  * ModelBuilderComponent
