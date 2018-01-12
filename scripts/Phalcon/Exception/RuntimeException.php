37ea6a996141cd284ad729885f6088e56825635f
sergeysviridenko <sergey.v.sviridenko@gmail.com>
2018-01-12 16:40:05 +0200

    Add option container and implements in model generator

---
+++ scripts/Phalcon/Exception/RuntimeException.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+  +------------------------------------------------------------------------+
+  | Phalcon Developer Tools                                                |
+  +------------------------------------------------------------------------+
+  | Copyright (c) 2011-present Phalcon Team (https://www.phalconphp.com)   |
+  +------------------------------------------------------------------------+
+  | This source file is subject to the New BSD License that is bundled     |
+  | with this package in the file LICENSE.txt.                             |
+  |                                                                        |
+  | If you did not receive a copy of the license and are unable to         |
+  | obtain it through the world-wide-web, please send an email             |
+  | to license@phalconphp.com so we can send you a copy immediately.       |
+  +------------------------------------------------------------------------+
+  | Authors: Sergii Svyrydenko <sergey.v.sviridenko@gmail.com              |
+  +------------------------------------------------------------------------+
+*/
+
+namespace Phalcon\Exception;
+
+class RuntimeException extends \RuntimeException
+{
+}
