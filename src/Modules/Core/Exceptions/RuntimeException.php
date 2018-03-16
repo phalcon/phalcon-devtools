d6a9b33d05f3b607498eddedbea2b4475f01502a
sergeysviridenko <sergey.v.sviridenko@gmail.com>
2018-03-16 11:15:02 +0200

    Add exceptions

---
+++ src/Modules/Core/Exceptions/RuntimeException.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+  +------------------------------------------------------------------------+
+  | Phalcon Developer Tools                                                |
+  +------------------------------------------------------------------------+
+  | Copyright (c) 2011-present Phalcon Team (https://www.phalconphp.com)   |
+  +------------------------------------------------------------------------+
+  | This source file is subject to the New BSD License that is bundled     |
+  | with this package in the file LICENSE.txt.                             |
+  |                                                                        |
+  | If you did not receive a copy of the license and are unable to         |
+  | obtain it through the world-wide-web, please send an email             |
+  | to license@phalconphp.com so we can send you a copy immediately.       |
+  +------------------------------------------------------------------------+
+  | Authors: Sergii Svyrydenko <sergey.v.sviridenko@gmail.com>             |
+  +------------------------------------------------------------------------+
+*/
+
+namespace Phalcon\Devtools\Modules\Core\Exceptions;
+
+use Phalcon\Exception as PhalconException;
+
+/**
+ * Phalcon\Devtools\Modules\Core\Exceptions\RuntimeException
+ *
+ * @package Phalcon\Devtools\Modules\Core\Exceptions
+ */
+class RuntimeException extends PhalconException
+{
+
+}
