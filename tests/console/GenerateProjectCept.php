1398a3669fd1451689a5379dbdb7ff9a9e6b9484
Anton Vasiliev <aionbase@gmail.com>
2019-10-27 15:15:03 +0000

    #1338 - Add test for project generation

---
+++ tests/console/GenerateProjectCept.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * @var Codeception\Scenario $scenario
+ */
+
+$I = new ConsoleTester($scenario);
+$I->wantToTest('Generating project');
+
+$output=<<<OUT
+Success: Project was successfully created.
+OUT;
+
+$projectsFolder = 'projects';
+
+$I->amInPath(app_path($projectsFolder));
+
+/**
+ * Case 1 - default path
+ */
+$projectName1 = 'project-tests1';
+$path1 = $projectsFolder . '/' . $projectName1;
+
+$I->dontSeeFileFound(app_path($path1));
+$I->runShellCommand('phalcon project ' . $projectName1);
+$I->seeFileFound(app_path($path1));
+$I->deleteDir(app_path($path1));
+
+/**
+ * Case 2 - custom path
+ */
+$projectName2 = 'project-tests2';
+$projectPath2 = 'project-tests2-custom';
+$path2 = $projectsFolder . '/' . $projectPath2;
+
+$I->dontSeeFileFound(app_path($path2));
+$I->runShellCommand("phalcon project $projectName2 simple $projectPath2");
+$I->seeFileFound(app_path($path2));
+$I->deleteDir(app_path($path2));
