1ee8289fb35965287e7f82f310140ab7ee64ffb6
Anton Vasiliev <aionbase@gmail.com>
2019-10-27 15:08:07 +0000

    #1338 - Add more one case test for project generation

--- tests/console/GenerateProjectCept.php
+++ tests/console/GenerateProjectCept.php
@@ -12,13 +12,28 @@ Success: Scaffold was successfully created.
 OUT;
 
 $projectsFolder = 'projects';
-$projectName = 'project-tests';
-$path = $projectsFolder . '/' . $projectName;
 
 $I->amInPath(app_path($projectsFolder));
-$I->dontSeeFileFound(app_path($path));
 
-$I->runShellCommand('phalcon project ' . $projectName);
+/**
+ * Case 1 - default path
+ */
+$projectName1 = 'project-tests1';
+$path1 = $projectsFolder . '/' . $projectName1;
 
-$I->seeFileFound(app_path($path));
-$I->deleteDir(app_path($path));
+$I->dontSeeFileFound(app_path($path1));
+$I->runShellCommand('phalcon project ' . $projectName1);
+$I->seeFileFound(app_path($path1));
+$I->deleteDir(app_path($path1));
+
+/**
+ * Case 2 - custom path
+ */
+$projectName2 = 'project-tests2';
+$projectPath2 = 'project-tests2-custom';
+$path2 = $projectsFolder . '/' . $projectPath2;
+
+$I->dontSeeFileFound(app_path($path2));
+$I->runShellCommand("phalcon project $projectName2 simple $projectPath2");
+$I->seeFileFound(app_path($path2));
+$I->deleteDir(app_path($path2));
