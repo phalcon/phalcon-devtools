b1a9111650d18dea0138d3f6a4fe0a498bffabee
Anton Vasiliev <aionbase@gmail.com>
2019-10-29 10:47:24 +0000

    #1336 - Add test case for project creation with --use-config-ini option [v4]

--- tests/console/GenerateProjectCept.php
+++ tests/console/GenerateProjectCept.php
@@ -25,6 +25,7 @@ $path1 = $projectsFolder . '/' . $projectName1;
 $I->dontSeeFileFound(app_path($path1));
 $I->runShellCommand('phalcon project ' . $projectName1);
 $I->seeFileFound(app_path($path1));
+$I->seeFileFound(app_path($path1 . '/app/config/config.php'));
 $I->deleteDir(app_path($path1));
 
 /**
@@ -37,4 +38,17 @@ $path2 = $projectsFolder . '/' . $projectPath2;
 $I->dontSeeFileFound(app_path($path2));
 $I->runShellCommand("phalcon project $projectName2 simple $projectPath2");
 $I->seeFileFound(app_path($path2));
+$I->seeFileFound(app_path($projectsFolder . '/' . $projectPath2 . '/' . $projectName2 . '/app/config/config.php'));
 $I->deleteDir(app_path($path2));
+
+/**
+ * Case 3 - ini config file
+ */
+$projectName3 = 'project-tests3';
+$path3 = $projectsFolder . '/' . $projectName3;
+
+$I->dontSeeFileFound(app_path($path3));
+$I->runShellCommand('phalcon project ' . $projectName3 . ' --use-config-ini');
+$I->seeFileFound(app_path($path3));
+$I->seeFileFound(app_path($path3 . '/app/config/config.ini'));
+$I->deleteDir(app_path($path3));
