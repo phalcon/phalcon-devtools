f74b68c7a50ec7e5db39aba5e97de7feb74e2676
Anton Vasiliev <aionbase@gmail.com>
2019-11-05 22:38:21 +0000

    #1341 - Change default value of 'baseUri' in templates

--- templates/project/modules/config.php
+++ templates/project/modules/config.php
@@ -23,11 +23,7 @@ return new \Phalcon\Config([
         'modelsDir'      => APP_PATH . '/common/models/',
         'migrationsDir'  => APP_PATH . '/migrations/',
         'cacheDir'       => BASE_PATH . '/cache/',
-
-        // This allows the baseUri to be understand project paths that are not in the root directory
-        // of the webpspace.  This will break if the public/index.php entry point is moved or
-        // possibly if the web server rewrite rules are changed. This can also be set to a static path.
-        'baseUri'        => preg_replace('/public([\/\\\\])index.php$/', '', $_SERVER["PHP_SELF"]),
+        'baseUri'        => '/',
     ],
 
     /**
