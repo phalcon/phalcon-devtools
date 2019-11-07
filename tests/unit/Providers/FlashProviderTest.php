4356c64604a1dbae1d606e19567c48e36fe22f52
Anton Vasiliev <aionbase@gmail.com>
2019-11-07 22:58:33 +0000

    #1356 - Implement FlashProvider provider

---
+++ tests/unit/Providers/FlashProviderTest.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types=1);
+
+namespace Phalcon\DevTools\Tests\Unit\Providers;
+
+use Codeception\Test\Unit;
+use Phalcon\DevTools\Providers\FlashProvider;
+use Phalcon\Di\ServiceProviderInterface;
+
+final class FlashProviderTest extends Unit
+{
+    public function testImplementation(): void
+    {
+        $class = $this->createMock(FlashProvider::class);
+
+        $this->assertInstanceOf(ServiceProviderInterface::class, $class);
+    }
+}
