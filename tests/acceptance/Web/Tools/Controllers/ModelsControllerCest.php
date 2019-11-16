846c506727e1312ffe594c8cfe719fa4e97639e5
Anton Vasiliev <aionbase@gmail.com>
2019-11-16 23:06:33 +0000

    #1355 - Implement ModelsControllerCest acceptance test

---
+++ tests/acceptance/Web/Tools/Controllers/ModelsControllerCest.php
@@ -0,0 +1,20 @@
+<?php
+declare(strict_types=1);
+
+namespace Phalcon\DevTools\Tests\Acceptance\Web\Tools\Controllers;
+
+use AcceptanceTester;
+
+final class ModelsControllerCest
+{
+    /**
+     * @covers \Phalcon\Devtools\Web\Tools\Controllers\ModelsController::indexAction
+     * @param AcceptanceTester $I
+     */
+    public function testIndexAction(AcceptanceTester $I): void
+    {
+        $I->amOnPage('/webtools.php/models/list');
+        $I->see('Models');
+        $I->see('All models that we managed to find');
+    }
+}
