850742e3140cfe562f7696c31b31a651c2b37f1b
Anton Vasiliev <aionbase@gmail.com>
2019-11-17 10:55:23 +0000

    #1355 - Implement test to generate controller

--- tests/acceptance/Web/Tools/Controllers/ControllersControllerCest.php
+++ tests/acceptance/Web/Tools/Controllers/ControllersControllerCest.php
@@ -28,4 +28,16 @@ final class ControllersControllerCest
         $I->see('Controllers');
         $I->see('Generate Controller');
     }
+
+    public function testSubmitGenerateAction(AcceptanceTester $I): void
+    {
+        $controllerName = 'TestControllerName';
+
+        $I->amOnPage('/webtools.php/controllers/generate');
+        $I->fillField('name', $controllerName);
+        $I->fillField('namespace', 'Test\WebTools');
+        $I->click('.form-horizontal input[type=submit]');
+        $I->see('All controllers that we managed to find');
+        $I->see($controllerName . 'Controller');
+    }
 }
