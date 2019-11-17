4bf4bc26ab946766847127c17095e1b8fbaad5fa
Anton Vasiliev <aionbase@gmail.com>
2019-11-17 22:12:34 +0000

    #1355 - Implement test to generate scaffold

--- tests/acceptance/Web/Tools/Controllers/ScaffoldControllerCest.php
+++ tests/acceptance/Web/Tools/Controllers/ScaffoldControllerCest.php
@@ -8,13 +8,41 @@ use AcceptanceTester;
 final class ScaffoldControllerCest
 {
     /**
-     * @covers \Phalcon\Devtools\Web\Tools\Controllers\ScaffoldController::indexAction
+     * @covers \Phalcon\Devtools\Web\Tools\Controllers\ScaffoldController::generateAction
      * @param AcceptanceTester $I
      */
-    public function testIndexAction(AcceptanceTester $I): void
+    public function testEnterGenerateAction(AcceptanceTester $I): void
     {
         $I->amOnPage('/webtools.php/scaffold/generate');
         $I->see('Scaffold');
         $I->see('Generate code from template');
+    }
+
+    public function testGenerateAction(AcceptanceTester $I): void
+    {
+        $namespace = 'Test\WebTools';
+
+        /**
+         * Generate Models
+         */
+        $I->amOnPage('/webtools.php/models/generate');
+
+        $modelsDir = $I->grabValueFrom('#modelsDir');
+        remove_dir($modelsDir);
+
+        $I->fillField('namespace', $namespace);
+        $I->checkOption('#force');
+        $I->click('input[type=submit]');
+
+        /**
+         * Generate rest of files
+         */
+        $I->amOnPage('/webtools.php/scaffold/generate');
+
+        $I->fillField('modelsNamespace', $namespace);
+        $I->checkOption('#force');
+        $I->click('input[type=submit]');
+        $I->see('Migrations');
+        $I->see('All migrations that we managed to find');
     }
 }
