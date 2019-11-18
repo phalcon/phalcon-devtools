ad98d6a1c98e27359d074b1388fd4dfb48943fd0
Anton Vasiliev <aionbase@gmail.com>
2019-11-18 22:47:26 +0000

    #1355 - Implement test to generate and run migrations

--- tests/acceptance/Web/Tools/Controllers/MigrationsControllerCest.php
+++ tests/acceptance/Web/Tools/Controllers/MigrationsControllerCest.php
@@ -22,11 +22,22 @@ final class MigrationsControllerCest
      * @covers \Phalcon\Devtools\Web\Tools\Controllers\MigrationsController::runAction
      * @param AcceptanceTester $I
      */
-    public function testRunAction(AcceptanceTester $I): void
+    public function testEnterRunAction(AcceptanceTester $I): void
     {
         $I->amOnPage('/webtools.php/migrations/run');
         $I->see('Migrations');
         $I->see('Run Migration');
+    }
+
+    /**
+     * @covers \Phalcon\Devtools\Web\Tools\Controllers\MigrationsController::generateAction
+     * @param AcceptanceTester $I
+     */
+    public function testEnterGenerateAction(AcceptanceTester $I): void
+    {
+        $I->amOnPage('/webtools.php/migrations/generate');
+        $I->see('Migrations');
+        $I->see('Generate Migration');
     }
 
     /**
@@ -38,5 +49,33 @@ final class MigrationsControllerCest
         $I->amOnPage('/webtools.php/migrations/generate');
         $I->see('Migrations');
         $I->see('Generate Migration');
+
+        $migrationsDir = $I->grabValueFrom('#migrationsDir');
+        remove_dir($migrationsDir);
+
+        $I->checkOption('#force');
+        $I->click('input[type=submit]');
+        $I->see('1.0.0');
+    }
+
+    /**
+     * @covers \Phalcon\Devtools\Web\Tools\Controllers\MigrationsController::runAction
+     * @param AcceptanceTester $I
+     */
+    public function testRunAction(AcceptanceTester $I): void
+    {
+        $this->testGenerateAction($I);
+
+        $I->amOnPage('/webtools.php/migrations/run');
+
+        $basePath = $I->grabValueFrom('#basePath');
+        $migrationVersion = $basePath . '/.phalcon/migration-version';
+        @unlink($migrationVersion);
+        file_put_contents($migrationVersion, '1.0.0');
+
+        $I->see('Migrations');
+        $I->see('Run Migration');
+        $I->click('input[type=submit]');
+        $I->see('Info: Everything is up to date');
     }
 }
