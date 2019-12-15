7922155b5787dd8ee8a2cb67500976893c1d2529
Anton Vasiliev <aionbase@gmail.com>
2019-12-15 22:04:14 +0000

    #1394 - Remove FlashProvider

--- tests/unit/Providers/FlashSessionProviderTest.php
+++ tests/unit/Providers/FlashSessionProviderTest.php
@@ -4,14 +4,14 @@ declare(strict_types=1);
 namespace Phalcon\DevTools\Tests\Unit\Providers;
 
 use Codeception\Test\Unit;
-use Phalcon\DevTools\Providers\FlashProvider;
+use Phalcon\DevTools\Providers\FlashSessionProvider;
 use Phalcon\Di\ServiceProviderInterface;
 
-final class FlashProviderTest extends Unit
+final class FlashSessionProviderTest extends Unit
 {
     public function testImplementation(): void
     {
-        $class = $this->createMock(FlashProvider::class);
+        $class = $this->createMock(FlashSessionProvider::class);
 
         $this->assertInstanceOf(ServiceProviderInterface::class, $class);
     }
