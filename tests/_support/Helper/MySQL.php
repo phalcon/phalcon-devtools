cebbe2cfb1aaa472af2518871aa2596c14a18984
Jeremy <pastouret.jeremy@gmail.com>
2020-04-02 20:53:35 +0200

    fix: force the mysql config setup

--- tests/_support/Helper/MySQL.php
+++ tests/_support/Helper/MySQL.php
@@ -1,11 +1,17 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Helper;
 
 use Codeception\Module;
 use Codeception\TestInterface;
+use PDO;
+use Phalcon\Config;
+use Phalcon\Db\Adapter\Pdo\AbstractPdo;
+use Phalcon\Db\Adapter\PdoFactory;
 
-class MySQL extends Module
+class Acceptance extends Module
 {
     /**
      * @var string
@@ -17,19 +23,20 @@ class MySQL extends Module
      */
     protected $projectPath;
 
+    /**
+     * @var AbstractPdo|null
+     */
+    protected static $phalconDb;
+
     public function _initialize()
     {
-        $this->projectPath = tests_path('_data/acceptance/' . $this->projectName);
-        /*$this->config = new \Phalcon\Config([
-            'database' => [
-                'adapter' => 'mysql',
-                'host' => getenv('MYSQL_TEST_DB_HOST'),
-                'port' => getenv('MYSQL_TEST_DB_PORT'),
-                'username' => getenv('MYSQL_TEST_DB_USER'),
-                'password' => getenv('MYSQL_TEST_DB_PASSWORD'),
-                'dbname' => getenv('MYSQL_TEST_DB_DATABASE'),
-            ],
-        ]);*/
+        $this->projectPath = tests_path('_data/mysql/' . $this->projectName);
+        /** @var AbstractPdo $db */
+        self::$phalconDb = (new PdoFactory())
+            ->newInstance(
+                'mysql',
+                $this->getDevToolsConfig()->get('database')->toArray()
+            );
     }
 
     public function _before(TestInterface $test)
@@ -44,5 +51,39 @@ class MySQL extends Module
         parent::_after($test);
 
         //shell_exec('rm -rf ' . $this->projectPath);
+    }
+
+    /**
+     * @return PDO
+     * @throws \Codeception\Exception\ModuleException
+     */
+    public function getDb(): PDO
+    {
+        return $this->getModule('Db')->_getDbh();
+    }
+
+    /**
+     * @return AbstractPdo
+     */
+    public function getPhalconDb(): AbstractPdo
+    {
+        return self::$phalconDb;
+    }
+
+    /**
+     * @return Config
+     */
+    public function getDevToolsConfig(): Config
+    {
+        return new Config([
+            'database' => [
+                'adapter' => 'mysql',
+                'host' => getenv('MYSQL_TEST_DB_HOST'),
+                'port' => getenv('MYSQL_TEST_DB_PORT'),
+                'username' => getenv('MYSQL_TEST_DB_USER'),
+                'password' => getenv('MYSQL_TEST_DB_PASSWORD'),
+                'dbname' => getenv('MYSQL_TEST_DB_DATABASE'),
+            ]
+        ]);
     }
 }
