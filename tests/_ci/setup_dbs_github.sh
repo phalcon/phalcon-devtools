5b8d9a2a4ebd54dbaff35bde18ef40e04cd4ae78
Jeremy <pastouret.jeremy@gmail.com>
2020-04-02 17:22:28 +0200

    fix: error init db

--- tests/_ci/setup_dbs_github.sh
+++ tests/_ci/setup_dbs_github.sh
@@ -9,15 +9,14 @@
 
 CURRENT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 TRAVIS_BUILD_DIR="${TRAVIS_BUILD_DIR:-$(dirname $(dirname $CURRENT_DIR))}"
-echo -e $POSTGRES_TEST_DB_PORT
 echo -e "Create PostgreSQL database..."
-psql -c 'create database devtools;' -U postgres
+psql -c 'create database devtools;' -U postgres -p $POSTGRES_TEST_DB_PORT
 psql -U postgres devtools -p $POSTGRES_TEST_DB_PORT -q -f "${TRAVIS_BUILD_DIR}/tests/_data/schemas/postgresql/dump.sql"
 echo -e "Done\n"
 
 echo -e "Create MySQL database..."
-mysql -u root -e "CREATE DATABASE IF NOT EXISTS devtools charset=utf8 collate=utf8_general_ci;"
-cat "${TRAVIS_BUILD_DIR}/tests/_data/schemas/mysql/dump.sql" | mysql -u root devtools
+mysql -u root -P $MYSQL_TEST_DB_PORT -e "CREATE DATABASE IF NOT EXISTS devtools charset=utf8 collate=utf8_general_ci;"
+cat "${TRAVIS_BUILD_DIR}/tests/_data/schemas/mysql/dump.sql" | mysql -u root devtools -P $MYSQL_TEST_DB_PORT
 echo -e "Done\n"
 
 wait
