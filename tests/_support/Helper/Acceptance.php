d300411ac13a8444ab64cd921c2ddb20488029de
Jeremy <pastouret.jeremy@gmail.com>
2020-04-03 11:43:33 +0200

    fix: hope fix some issue with test

--- tests/_support/Helper/Acceptance.php
+++ tests/_support/Helper/Acceptance.php
@@ -32,7 +32,7 @@ class Acceptance extends Module
         if (true === in_array($driver, [ 'mysql', 'pgsql' ])) {
             copy(PATH_DATA . 'acceptance' .
                 DIRECTORY_SEPARATOR . $driver .
-                DIRECTORY_SEPARATOR . 'config.php', PROJECT_PATH . 'webtools/app/config/config.php');
+                DIRECTORY_SEPARATOR . 'config.php', 'webtools/app/config/config.php');
         }
 
         parent::_before($test);
