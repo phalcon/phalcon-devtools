d441542d289a34fdef2a7742f53d08429a23d4cd
Jeremy <pastouret.jeremy@gmail.com>
2020-04-03 14:23:11 +0200

    fix: try debug with scandir

--- tests/_support/Helper/Acceptance.php
+++ tests/_support/Helper/Acceptance.php
@@ -29,13 +29,11 @@ class Acceptance extends Module
             $this->driver = $driver;
         }
 
-        print_r(scandir(getenv('WEB_TOOLS_PROJECT')));
+      /*  print_r(scandir(getenv('WEB_TOOLS_PROJECT')));
         echo '-----';
         print_r(scandir(getenv('HOME')));
-        echo '-----';
+        echo '-----';*/
         print_r(scandir(PROJECT_PATH));
-        echo '-----';
-        print_r(scandir(TESTS_PATH));
         echo '-----';
         print_r(scandir(TESTS_PATH));
         die;
