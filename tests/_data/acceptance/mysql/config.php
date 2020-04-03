9565e5038d02cd5f1ee3e144053c3a2e2d0262bf
Jeremy <pastouret.jeremy@gmail.com>
2020-04-03 11:16:53 +0200

    fix: hope fix some issue with test

---
+++ tests/_data/acceptance/mysql/config.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+ * Modified: prepend directory path of current file, because of this file own different ENV under between Apache and command line.
+ * NOTE: please remove this comment.
+ */
+defined('BASE_PATH') || define('BASE_PATH', getenv('BASE_PATH') ?: realpath(dirname(__FILE__) . '/../..'));
+defined('APP_PATH') || define('APP_PATH', BASE_PATH . '/app');
+
+return new \Phalcon\Config([
+    'database' => [
+        'adapter'     => 'Mysql',
+        'host'     => env('DATA_MYSQL_HOST'),
+        'username' => env('DATA_MYSQL_USER'),
+        'password' => env('DATA_MYSQL_PASS'),
+        'dbname'   => env('DATA_MYSQL_NAME'),
+        'port'     => env('DATA_MYSQL_PORT'),
+        'charset'  => env('DATA_MYSQL_CHARSET')
+    ],
+    'application' => [
+        'appDir'         => APP_PATH . '/',
+        'controllersDir' => APP_PATH . '/controllers/',
+        'modelsDir'      => APP_PATH . '/models/',
+        'migrationsDir'  => APP_PATH . '/migrations/',
+        'viewsDir'       => APP_PATH . '/views/',
+        'pluginsDir'     => APP_PATH . '/plugins/',
+        'libraryDir'     => APP_PATH . '/library/',
+        'cacheDir'       => BASE_PATH . '/cache/',
+        'baseUri'        => '/',
+    ]
+]);
---
+++ tests/_data/acceptance/pgsql/config.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+ * Modified: prepend directory path of current file, because of this file own different ENV under between Apache and command line.
+ * NOTE: please remove this comment.
+ */
+defined('BASE_PATH') || define('BASE_PATH', getenv('BASE_PATH') ?: realpath(dirname(__FILE__) . '/../..'));
+defined('APP_PATH') || define('APP_PATH', BASE_PATH . '/app');
+
+return new \Phalcon\Config([
+    'database' => [
+        'adapter'     => 'Mysql',
+        'host'     => env('DATA_POSTGRES_HOST'),
+        'username' => env('DATA_POSTGRES_USER'),
+        'password' => env('DATA_POSTGRES_PASS'),
+        'port'     => env('DATA_POSTGRES_PORT'),
+        'dbname'   => env('DATA_POSTGRES_NAME'),
+        'schema'   => env('DATA_POSTGRES_SCHEMA'),
+    ],
+    'application' => [
+        'appDir'         => APP_PATH . '/',
+        'controllersDir' => APP_PATH . '/controllers/',
+        'modelsDir'      => APP_PATH . '/models/',
+        'migrationsDir'  => APP_PATH . '/migrations/',
+        'viewsDir'       => APP_PATH . '/views/',
+        'pluginsDir'     => APP_PATH . '/plugins/',
+        'libraryDir'     => APP_PATH . '/library/',
+        'cacheDir'       => BASE_PATH . '/cache/',
+        'baseUri'        => '/',
+    ]
+]);
