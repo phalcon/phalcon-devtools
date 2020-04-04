0072e818a5482fea5a579b471c1811df6196233e
Jeremy <pastouret.jeremy@gmail.com>
2020-04-04 23:33:13 +0200

    fix: go back2

---
+++ tests/debug.php
@@ -0,0 +1,22 @@
+<?php
+$config =  '../webtools/app/config/config.php';
+
+$class = 'Phalcon\Db\Adapter\Pdo\\' . $config->database->adapter;
+
+$params = [
+'host'     => $config->database->host,
+'username' => $config->database->username,
+'password' => $config->database->password,
+'dbname'   => $config->database->dbname,
+'charset'  => $config->database->charset
+];
+
+if ($config->database->adapter == 'Postgresql') {
+unset($params['charset']);
+}
+var_dump($params);
+var_dump($config);
+var_dump($class);
+var_dump(new $class($params));
+
+?>
