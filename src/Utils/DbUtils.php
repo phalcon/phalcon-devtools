4e34fa3de28690790568f119a25434b13a5cebe8
Jeremy <pastouret.jeremy@gmail.com>
2020-04-04 22:29:08 +0200

    fix: go back

--- src/Utils/DbUtils.php
+++ src/Utils/DbUtils.php
@@ -30,11 +30,10 @@ class DbUtils extends Injectable
     public function listTables(bool $all = false, $connection = 'db'): array
     {
         $tables = $all ? ['@' => 'all'] : [];
-        var_dump($connection);
 
         if ($this->getDI()->has($connection)) {
             $connection = $this->getDI()->getShared($connection);
-            var_dump($connection);die;
+
             $dbTables = $connection->listTables();
             foreach ($dbTables as $dbTable) {
                 $tables[$dbTable] = $dbTable;
