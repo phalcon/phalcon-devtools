b95cfa900a654efee19f96dfaf73a25e77b06b9d
Jeremy <pastouret.jeremy@gmail.com>
2020-04-06 19:07:29 +0200

    fix: issue with script

--- tests/_ci/setup_dbs_github.sh
+++ tests/_ci/setup_dbs_github.sh
@@ -14,7 +14,7 @@ psql -U postgres devtools -p "$POSTGRES_DB_PORT" -h localhost -q -f "${BUILD_DIR
 echo -e "Done\n"
 
 echo -e "Create MySQL database..."
-< "${BUILD_DIR}/tests/_data/schemas/mysql/dump.sql" | mysql --host=127.0.0.1 --user=root --password=root --port="$MYSQL_DB_PORT" --database devtools
+cat "${BUILD_DIR}/tests/_data/schemas/mysql/dump.sql" | mysql --host=127.0.0.1 --user=root --password=root --port="$MYSQL_DB_PORT" --database devtools
 
 echo -e "Check database created"
 mysql --host=127.0.0.1 --user=root --password=root --port="$MYSQL_DB_PORT" -e 'show databases;'
