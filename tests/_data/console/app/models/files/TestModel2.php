e51128edb7410cf3be70e4b58937472537b1d610
M4nu <m4nu.coder@gmail.com>
2024-06-11 21:46:43 -0500

    fixing docblock test model

--- tests/_data/console/app/models/files/TestModel2.php
+++ tests/_data/console/app/models/files/TestModel2.php
@@ -21,6 +21,8 @@ class TestModel2 extends \Phalcon\Mvc\Model
 
     /**
      * Initialize method for model.
+     *
+     * @return void
      */
     public function initialize()
     {
@@ -32,6 +34,7 @@ class TestModel2 extends \Phalcon\Mvc\Model
      * Allows to query a set of records that match the specified conditions
      *
      * @param mixed $parameters
+     *
      * @return TestModel2[]|TestModel2|\Phalcon\Mvc\Model\ResultSetInterface
      */
     public static function find($parameters = null): \Phalcon\Mvc\Model\ResultsetInterface
@@ -43,6 +46,7 @@ class TestModel2 extends \Phalcon\Mvc\Model
      * Allows to query the first record that match the specified conditions
      *
      * @param mixed $parameters
+     *
      * @return TestModel2|\Phalcon\Mvc\Model\ResultInterface|\Phalcon\Mvc\ModelInterface|null
      */
     public static function findFirst($parameters = null): ?\Phalcon\Mvc\ModelInterface
--- tests/_data/console/app/models/files/TestModel3.php
+++ tests/_data/console/app/models/files/TestModel3.php
@@ -21,6 +21,8 @@ class TestModel3 extends \Phalcon\Mvc\Model
 
     /**
      * Initialize method for model.
+     *
+     * @return void
      */
     public function initialize()
     {
@@ -32,6 +34,7 @@ class TestModel3 extends \Phalcon\Mvc\Model
      * Allows to query a set of records that match the specified conditions
      *
      * @param mixed $parameters
+     *
      * @return TestModel3[]|TestModel3|\Phalcon\Mvc\Model\ResultSetInterface
      */
     public static function find($parameters = null): \Phalcon\Mvc\Model\ResultsetInterface
@@ -43,6 +46,7 @@ class TestModel3 extends \Phalcon\Mvc\Model
      * Allows to query the first record that match the specified conditions
      *
      * @param mixed $parameters
+     *
      * @return TestModel3|\Phalcon\Mvc\Model\ResultInterface|\Phalcon\Mvc\ModelInterface|null
      */
     public static function findFirst($parameters = null): ?\Phalcon\Mvc\ModelInterface
